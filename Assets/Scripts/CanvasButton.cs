793f123351e031607d03ae21300b458a1efc2bad
artem <artem-prokofiev@yandex.ru>
2021-07-23 15:45:46 +0300

    added game

---
+++ Assets/Scripts/CanvasButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasButton : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadInstagram()
+    {
+        Application.OpenURL("");
+    }
+
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float speed = 5f;
+    private Transform _rotator;
+
+     private void Start()
+    {
+        _rotator = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        _rotator.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
